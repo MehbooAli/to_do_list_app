59d0b48457a57e7e1b5676cf58c72db43daa5d6a
Mehboob Ali <mehboobali.87187@gmail.com>
2023-10-01 23:10:03 +0500

    Start project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_list_app/task_page.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TaskPage(),
+    );
+  }
+}
